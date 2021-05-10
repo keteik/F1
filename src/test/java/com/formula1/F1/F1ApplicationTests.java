577a60294614d77b3b846e476657c1d7cf9f9f7b
keteik <ki3t3k@gmail.com>
2021-05-10 20:43:36 +0200

    Initial commit

---
+++ src/test/java/com/formula1/F1/F1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formula1.F1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
